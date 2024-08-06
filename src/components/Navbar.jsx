adca9a6a48c0cc46631699e9f10de0ac8ea7fb9a
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-08-06 20:45:01 +0530

    project setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-const navbar = () => {
+const Navbar = () => {
   return (
-    <div>navbar</div>
-  )
-}
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">LOGO</NavLink>
+        </li>
+        <li>
+          <NavLink to="/">Products</NavLink>
+        </li>
+        <li>
+          <NavLink to="/">Infrastructure</NavLink>
+        </li>
+        <li>
+          <NavLink to="/">Quality Assurance</NavLink>
+        </li>
+        <li>
+          <NavLink to="/">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/">ENG</NavLink>
+        </li>
+        <li>
+          <NavLink to="/">BUTTON</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default navbar
+export default Navbar;
